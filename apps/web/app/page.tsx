b1de3d5889b44ec4861b1a331d655e023d6537fb
Harsh <harshpandey2060@gmail.com>
2024-09-16 11:16:12 +0530

    Socket_io context api setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,23 +4,24 @@ import { useSocket } from "../context/SocketProvider"
 import classes from "./page.module.css"
 
 
-export default function Page(){
+export default function Page() {
   const {sendMessage} = useSocket()
   const [message, setMessage] = useState('')
 
-return(
-  <div>
+  return (
     <div>
-  <h1>
-    All Messages will appear here</h1>
+      <div>
+        <h1>
+          All Messages will appear here</h1>
+      </div>
+      <div>
+        <input className={classes["chat-input"]} type="text" placeholder="message..."
+        onChange={(e) => setMessage(e.target.value)}
+        />
+        <button className={classes["button"]}
+        onClick={e => sendMessage(message)}
+        >Send</button>
+      </div>
     </div>
-    <div>
-      <input className={classes["chat-input"]} type="text" placeholder = "message..." 
-      onChange={(e) => setMessage(e.target.value)}/>
-      <button className={classes["button"]}
-      onClick={(e)=> sendMessage(message)}
-      >Send</button>
-    </div>
-  </div>
-)
+  )
 }
